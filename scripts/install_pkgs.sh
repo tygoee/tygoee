0b30c8af10f931fbffde0340c3dcc74dd259bf4a
tygoee <tygoee@outlook.com>
2023-11-02 12:12:11 +0100

    Add cork to install_pkgs.sh

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -30,3 +30,28 @@ sudo apt install -y ~/Downloads/Minecraft.deb
 
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 sudo apt install -y speedtest
+
+# Wine
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
+sudo apt update
+
+sudo apt install --install-recommends winehq-stable
+
+# Proton
+mkdir -p ~/.local/lib/
+wget -P ~/.local/lib/ https://github.com/GloriousEggroll/wine-ge-custom/releases/download/GE-Proton8-22/wine-lutris-GE-Proton8-22-x86_64.tar.xz
+tar -xJf ~/.local/lib/wine-lutris-GE-Proton8-22-x86_64.tar.xz -C ~/.local/lib/
+
+# Cork
+sudo apt install git build-essential cmake
+sudo apt install libboost-all-dev libzip-dev zlib1g-dev libbz2-dev liblzma-dev libssl-dev curl libcurl4-openssl-dev liblua5.4-dev
+git clone https://github.com/CorkHQ/Cork.git
+cd Cork || :
+mkdir build
+cmake -Bbuild -H. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCPR_USE_SYSTEM_CURL=ON
+cd build || :
+cmake --build . --target all
+cmake --install . --prefix ~/.local
